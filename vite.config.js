2fa3ff58d8de6f452df4ef1e1e4a18a530239707
Davi Soares <davi_guaglianip@outlook.com>
2025-07-04 11:03:47 -0300

    deploy configs added

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/LavaJato-serviceOrder-app"
+  base: process.env.VITE_BASE_PATH || "/LavaJato-serviceOrder-app"
 })
