400daef014f8361385a60f0a2a0195d962cefe76
Davi Soares <davi_guaglianip@outlook.com>
2025-07-09 11:48:13 -0300

    trash section no items finded label added

--- src/components/TrashListSection.jsx
+++ src/components/TrashListSection.jsx
@@ -134,7 +134,7 @@ function TrashListSection({ deletedOrders, onRestore, onPermanentDelete }) {
 			</div>
 
 			{ordersFiltradas.length === 0 ? (
-				<p className="text-center text-gray-400">Nenhuma ordem na lixeira.</p>
+				<p className="text-center text-gray-400">Nenhuma ordem encontrada.</p>
 			) : (
 				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
 					{ordersFiltradas
